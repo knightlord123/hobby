e8278c40a8b5597d5edadba626117d44b63e11c1
knightlord123 <59772049+knightlord123@users.noreply.github.com>
2022-01-29 14:17:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/my_db');
+var personSchema = mongoose.Schema({
+   name: String,
+   age: Number,
+   hobby: String
+});
+var Person = mongoose.model("Person", personSchema);
+
+
+app.get('/', function(req, res){
+   res.render('person');
+});
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array()); 
+app.use(express.static('public'));
+
+app.post('/', function(req, res){
+   console.log("A request for things received at the form" )
+   var personInfo = req.body; //Get the parsed information
+   
+   if(!personInfo.name || !personInfo.age || !personInfo.hobby){
+      res.render('show_message', {
+         message: "Sorry, you provided worng info", type: "error"});
+   } else {
+      var newPerson = new Person({
+         name: personInfo.name,
+         age: personInfo.age,
+         nationality: personInfo.hobby
+      });
+		
+      newPerson.save(function(err, Person){
+         if(err)
+            res.render('show_message', {message: "Database error", type: "error"});
+         else
+            res.render('show_message', {
+               message: "New person added", type: "success", person: personInfo});
+      });
+   }
+});
+app.listen(3000);
